678b1d71aaba128712358e349f483e99d8361fa3
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-04-17 12:14:57 +0530

    depoly

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route path='/' component={Home} exact/>
+        <Route path='/chats' component={Chat}/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,58 @@
+
+
+import React from 'react';
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react';
+import Login from '../components/Auth/Login';
+import Signup from '../components/Auth/Signup';
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+
+
+const Home = () => {
+   const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg="black"
+        textColor="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text>
+          Chattt
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
+
